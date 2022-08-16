f0e61f9e85a94ff5ded6e3ffb1a55b499d292239
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2022-08-16 21:36:39 +0100

    web scraper app

---
+++ payporte.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+
+	file, err := os.Create("porte002.csv")
+	if err != nil {
+		log.Fatalf("Could not create file: %q\n", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("payporte.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL)
+	})
+
+	c.OnHTML(".product-item", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText(".product-item-info > .product-item-details > h2 > a"),
+			e.ChildText(".product-item-info > .product-item-details > .price-box > .normal-price > .price-container > .price-wrapper > .price"),
+		})
+	})
+
+	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.Attr("src"),
+		})
+	})
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Scraping Page: %d\n", i)
+
+		c.Visit("https://payporte.com/clearance-sales.html?p=" + strconv.Itoa(i))
+	}
+
+	log.Println(c)
+}
